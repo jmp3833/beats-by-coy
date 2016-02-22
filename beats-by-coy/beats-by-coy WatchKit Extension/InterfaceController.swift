2bcb2405fd93c0baa31ce56e131a1967b5112d80
jmp3833 <jmp3833@rit.edu>
2016-02-22 09:27:16 -0500

    add initial project config

---
+++ beats-by-coy/beats-by-coy WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  beats-by-coy WatchKit Extension
+//
+//  Created by Justin on 2/22/16.
+//  Copyright © 2016 Don't Believe Me, Just Watch. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
